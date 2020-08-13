f6ff8d8035f1e637fca210844c1ba4be5b9efda1
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-13 12:58:31 +0200

    Models Routes and Controllers done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+response = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredients = JSON.parse(response)
+ingredients["drinks"].each do |element|
+  Ingredient.create(name: element['strIngredient1'])
+end
+
+puts "Done."
